d03321d3fc2f2a521dca5cc538165667b24a7012
sbx_user1059 <sbx_user1059@ip-10-128-116-249.eu-west-2.compute.internal>
2018-09-06 08:46:19 +0000

    Thu Sep 06 08:46:19 UTC 2018

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -5,22 +5,13 @@ import befaster.runner.SolutionNotImplementedException;
 public class SumSolution {
 
     public int compute(int x, int y) {
-
+        if (numberInRange(x) && numberInRange(y))
+            return x + y;
         throw new SolutionNotImplementedException();
     }
 
     private boolean numberInRange(int number){
         return number >= 0 && number <= 100;
-    }
-
-    public static void main(String[] args) {
-        SumSolution sumSolution = new SumSolution();
-        System.out.println(sumSolution.numberInRange(0));
-        System.out.println(sumSolution.numberInRange(-1));
-        System.out.println(sumSolution.numberInRange(23));
-        System.out.println(sumSolution.numberInRange(100));
-        System.out.println(sumSolution.numberInRange(101));
-
     }
 
 }
