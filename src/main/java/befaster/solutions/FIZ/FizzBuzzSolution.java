01c15e78799735d147c5f68922fa26db6b017ea7
sbx_user1060 <sbx_user1060@ip-10-128-35-49.eu-west-2.compute.internal>
2018-09-06 09:31:00 +0000

    Thu Sep 06 09:31:00 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,8 +5,19 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
+        StringBuilder result = new StringBuilder();
+        String fizz = "fizz";
+        String buzz = "buzz";
+        String num = "1";
+
         if (number % 3 == 0)
-        throw new SolutionNotImplementedException();
+            result.append(fizz);
+        if (number % 5 == 0)
+            result.append(buzz);
+        if (number % 3 != 0 && number % 5 != 0)
+            result.append(num);
+        return result.toString();
+//        throw new SolutionNotImplementedException();
     }
 
 }
--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -1,4 +1,25 @@
 package befaster.solutions.FIZ;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class FizzBuzzSolutionTest {
+    FizzBuzzSolution fizzBuzzSolution;
+
+    @Before
+    public void init(){
+        fizzBuzzSolution = new FizzBuzzSolution();
+    }
+
+    @Test
+    public void fizzBuzzTest(){
+        assertEquals("fizz", fizzBuzzSolution.fizzBuzz(3));
+        assertEquals("fizz", fizzBuzzSolution.fizzBuzz(12));
+
+        assertEquals("buzz", fizzBuzzSolution.fizzBuzz(5));
+        assertEquals("buzz", fizzBuzzSolution.fizzBuzz(25));
+
+        assertEquals("fizzBuzz", fizzBuzzSolution.fizzBuzz(30));
+    }
 }
